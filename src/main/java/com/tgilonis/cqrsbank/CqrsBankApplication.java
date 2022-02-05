c8f911205020b773aa3b21809839034aaa4650c0
tgilonis <tgilonis@protonmail.com>
2022-02-05 12:01:14 +0000

    Initial commit - adding spring initializer project

---
+++ src/main/java/com/tgilonis/cqrsbank/CqrsBankApplication.java
@@ -0,0 +1,13 @@
+package com.tgilonis.cqrsbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CqrsBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CqrsBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tgilonis/cqrsbank/CqrsBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgilonis.cqrsbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CqrsBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
