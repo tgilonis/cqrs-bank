ab132b36c3e8a711b7b15a8e21fadef2006e2052
tgilonis <tgilonis@protonmail.com>
2022-02-06 16:02:46 +0000

    Added base commands

--- src/main/java/com/tgilonis/cqrsbank/command/aggregate/AccountAggregate.java
+++ src/main/java/com/tgilonis/cqrsbank/command/aggregate/AccountAggregate.java
@@ -1,9 +1,14 @@
 package com.tgilonis.cqrsbank.command.aggregate;
 
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
 import java.math.BigDecimal;
 
+@Aggregate
 public class AccountAggregate
 {
+    @AggregateIdentifier
     private String accountId;
     private BigDecimal balance;
     private String status;
---
+++ src/main/java/com/tgilonis/cqrsbank/command/command/CreateAccountCommand.java
@@ -0,0 +1,18 @@
+package com.tgilonis.cqrsbank.command.command;
+
+import java.math.BigDecimal;
+
+public class CreateAccountCommand extends BaseCommand<String> {
+
+    private final BigDecimal balance;
+
+    public CreateAccountCommand(String id, BigDecimal balance) {
+        super(id);
+        this.balance = balance;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+}
+
